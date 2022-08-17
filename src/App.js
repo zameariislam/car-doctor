23a50ba0afea6f9648611243d3b4fe1bfc5b401a
zameariislam <zameariislam@gmail.com>
2022-08-17 21:54:53 +0200

    load fake data,map through services to display single service

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 
 import './App.css';
 import Footer from './Shared/Footer/Footer';
+import { Routes, Route} from "react-router-dom";
+import About from './Pages/About/About';
+import Home from './Pages/Home/Home/Home';
+import Header from './Shared/Header/Header';
 
 function App() {
   return (
-    <div className="App">
+    <div >
+      <Header/>
+
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/about' element={<About/>}></Route>
+      </Routes>
       <Footer/>
+
+      
+     
     
     </div>
   );
---
+++ src/Pages/Home/Home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Services from '../Services/Services';
+
+
+const Home = () => {
+    return (
+        <div>
+            <Services/>
+            
+          
+            
+        </div>
+    );
+};
+
+export default Home;
