5a18d8c4af69121d0d7e872915e521254474c8b9
zameariislam <zameariislam@gmail.com>
2022-08-18 13:47:29 +0200

    email-password based authentication

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,35 +1,49 @@
 import React from 'react';
 import './Register.css'
-import { Link,useNavigate } from "react-router-dom";
+import { Link,useNavigate, } from "react-router-dom";
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init';
 
 const Register = () => {
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error,
+      ] = useCreateUserWithEmailAndPassword(auth);
     const navigate = useNavigate();
 
     const navigateLogin = () =>{
         navigate('/login');
     }
 
-    const handleRegister=(e)=>{
-        e.preventDefault();
-      const name=e.target.name.value;
-      const email=e.target.email.value;
-      const password=e.target.password.value;
-     
-
+    if(user){
+        navigate('/home');
     }
+
+    const handleRegister = event =>{
+        event.preventDefault();
+        const name = event.target.name.value;
+        const email = event.target.email.value;
+        const password = event.target.password.value;
+
+        createUserWithEmailAndPassword(email, password);
+        console.log(error)
+    }
+
     return (
         <div className='register-form'>
-        <h2 style={{textAlign: 'center'}}>Please Register</h2>
-        <form onSubmit={handleRegister} >
-            <input type="text" name="name" id="" placeholder='Your Name'/>
-            
-            <input type="email" name="email" id="" placeholder='Email Address' required/>
-            
-            <input type="password" name="password" id="" placeholder='Password' required/>
-            <input type="submit" value="Register" />
-        </form>
-        <p>Already have an account? <Link to="/login" className='text-danger pe-auto text-decoration-none' onClick={navigateLogin}>Please Login</Link> </p>
-    </div>
+            <h2 style={{textAlign: 'center'}}>Please Register</h2>
+            <form onSubmit={handleRegister}>
+                <input type="text" name="name" id="" placeholder='Your Name'/>
+                
+                <input type="email" name="email" id="" placeholder='Email Address' required/>
+                
+                <input type="password" name="password" id="" placeholder='Password' required/>
+                <input type="submit" value="Register" />
+            </form>
+            <p>Already have an account? <Link to="/login" className='text-danger pe-auto text-decoration-none' onClick={navigateLogin}>Please Login</Link> </p>
+        </div>
     );
 };
 
--- src/firebase.init.js
+++ src/firebase.init.js
@@ -1,22 +1,20 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp,getAuth } from "firebase/app";
-
-
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey:process.env.REACT_KEY_apiKey,
-  authDomain:process.env.REACT_KEY_authDomain,
-  projectId:process.env.REACT_KEY_projectId, 
-  storageBucket:process.env.REACT_KEY_storageBucket, 
-  messagingSenderId:process.env.REACT_KEY_messagingSenderId,
-  appId:process.env.REACT_KEY_appId=1, 
+  apiKey: "AIzaSyAS0bqjfI2bXmesGWk7kigRdTg5YPtSll8",
+  authDomain: "car-doctor-f5375.firebaseapp.com",
+  projectId: "car-doctor-f5375",
+  storageBucket: "car-doctor-f5375.appspot.com",
+  messagingSenderId: "348910964054",
+  appId: "1:348910964054:web:e2f7c15abb9839de461af7"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth=getAuth(app)
-
 export default auth;
